99799fd5a48f8d27a7118bdca0b65b549671dc66
vinelouzada <vinelouzada@gmail.com>
2021-11-06 12:09:41 -0300

    Adicionando banner e filmes na pagina principal

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  require "config/filmes.php";
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -11,7 +15,7 @@
       href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/index.css" />
     <title>PopCINE</title>
   </head>
   <body>
@@ -24,7 +28,27 @@
         </ul>
       </nav>
     </header>
-    <main></main>
+    <main>
+      <div class="container-banner">
+          <img src="img/banner-site.png" alt="banner site">
+      </div>
+      <div class="container-main">
+        <h2>Agora no Cinema</h2>
+      </div>
+      <div class="container-main container-filmes">
+        <?php foreach($filmes as $filme){?>
+            <ul>
+                <li><img src="<?= $filme['capa_filme']?>" alt="capa filme"></li>
+                <li><?= $filme['nome_filme']?></li>
+                <li><?= $filme['duracao']?></li>
+                <li><?= $filme['idade_indicada']?></li>
+            </ul>
+        <?php }?>
+      </div>
+
+    </main>
+
+    
     <footer></footer>
   </body>
 </html>
